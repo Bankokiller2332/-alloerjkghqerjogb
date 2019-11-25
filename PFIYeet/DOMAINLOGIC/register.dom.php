249de9b0c8a242451490cefb99c145d3b01f84cd
Bankokiller2332 <guillaume.paquin2332@gmail.com>
2019-11-25 12:32:02 -0500

    registerview a changer

--- PFIYeet/DOMAINLOGIC/register.dom.php
+++ PFIYeet/DOMAINLOGIC/register.dom.php
@@ -15,7 +15,28 @@
     $username = $_POST["username"];
     $pw = $_POST["pw"];
     $pwv = $_POST["pwValidation"];
+    $url = $_FILES;
+    
+    if(!is_null($picture)
+    {
+       $img_extensions_arr = array("jpg","jpeg","png","gif");
+       if(!in_array($media_file_type, $img_extensions_arr))
+       {
+            header("Location: ../error.php?ErrorMSG=TYPE DE PHOTO INVALIDE");
+            die();
+        }
 
+        $media_file_type = pathinfo($_FILES['Media']['name'] ,PATHINFO_EXTENSION); 
+        $path = tempnam("../PHOTO/profil", '');
+        unlink($path);
+        $file_name = basename($path, ".tmp");
+        $url = $target_dir . $file_name . "." . $media_file_type;
+        move_uploaded_file($_FILES['Media']['tmp_name'], "../" . $url);
+    }
+    else{
+        $url = "./PHOTO/AvatarParDefaut.png";
+    }
+    
     //Validation Posts
     if(!Validator::validate_email($email) || !Validator::validate_password($pw))
     {
@@ -27,7 +48,7 @@
     $aUser = new User();
 
     //validateLogin
-    if(!$aUser->register($email, $username, $pw, $pwv))
+    if(!$aUser->register($email, $username, $pw, $pwv, $picture))
     {
         http_response_code(400);
         header("Location: ../error.php?ErrorMSG=invalid email or password");
