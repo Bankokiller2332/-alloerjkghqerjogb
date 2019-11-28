3462d131c71d20a70012b3b1edc8973744e8b21f
201537747 <201537747@P116-18>
2019-11-28 16:26:17 -0500

    Update register.dom.php

--- PFIYeet/DOMAINLOGIC/register.dom.php
+++ PFIYeet/DOMAINLOGIC/register.dom.php
@@ -17,7 +17,6 @@
     $username = $_POST["username"];
     $pw = $_POST["pw"];
     $pwv = $_POST["pwValidation"];
-<<<<<<< HEAD
     $media_file_type = pathinfo($_FILES['Media']['name'] ,PATHINFO_EXTENSION);
 
     $img_extensions_arr = array("jpg","jpeg","png","gif");    
@@ -50,26 +49,6 @@
    
     $url = $_FILES;
     
-    if(!is_null($picture)
-    {
-       $img_extensions_arr = array("jpg","jpeg","png","gif");
-       if(!in_array($media_file_type, $img_extensions_arr))
-       {
-            header("Location: ../error.php?ErrorMSG=TYPE DE PHOTO INVALIDE");
-            die();
-        }
->>>>>>> 2be3646222488006ca858a2e2d8b56ff704b16ca
-
-        $media_file_type = pathinfo($_FILES['Media']['name'] ,PATHINFO_EXTENSION); 
-        $path = tempnam("../PHOTO/profil", '');
-        unlink($path);
-        $file_name = basename($path, ".tmp");
-        $url = $target_dir . $file_name . "." . $media_file_type;
-        move_uploaded_file($_FILES['Media']['tmp_name'], "../" . $url);
-    }
-    else{
-        $url = "./PHOTO/AvatarParDefaut.png";
-    }
     
     //Validation Posts
     if(!Validator::validate_email($email) || !Validator::validate_password($pw))
