dce7de8481694215c290e4c15ef0536afe0eaf62
Vincent Roy-St-Jean <201859369@edu.clg.qc.ca>
2019-12-09 12:28:54 -0500

    ???

--- PFIYeet/DOMAINLOGIC/showImage.dom.php
+++ PFIYeet/DOMAINLOGIC/showImage.dom.php
@@ -5,13 +5,13 @@
     $albumid = $_GET["albumID"];
     $albumTitle = $_GET["albumTitle"];
 
-    $aAlbum = new Album();
-    
+    $aAlbum = new Album();  
     $Image = new Image();
     $lstImage = $Image->create_image_list($albumid); 
 
    // $lst = $aAlbum->load_image();
     echo "<h1> $albumTitle </h1>";
+    if($_SESSION["userName"] == $lstImage)  
     foreach($lstImage as $img){
         $img->display();
     }
