73d167951460fcfc4af9ca34d87d32f163bd46df
ViStRoy <57631640+ViStRoy@users.noreply.github.com>
2019-12-18 15:38:19 -0500

    yeet

--- PFIYeet/CLASSES/IMAGE/image.php
+++ PFIYeet/CLASSES/IMAGE/image.php
@@ -89,7 +89,6 @@ class Image{
     }
 
     public function display(){
-<<<<<<< HEAD
         $image = new Image();
         $image->set_id($this->id);
         $image->set_albumID($this->albumID);
@@ -97,9 +96,6 @@ class Image{
         $image->set_description($this->description);
         $image->set_tempsCreation($this->tempsCreation);       
         return $image;
-=======
-     
->>>>>>> 0f9a3b65c1002af816ceea23fa033081304b6ff1
     }
 
     public function addImage($url, $albumID, $description){
